43aaf196eb70338a68cd572ef2df89b57e05276d
JhoranPablo <jhoranpablodev@gmail.com>
2023-07-07 18:46:42 -0300

    alteração no valor max e min

--- script.js
+++ script.js
@@ -4,9 +4,23 @@ function generateNumber(){
     const max = Math.floor(document.querySelector(".max").value)
     const resultRandom = document.querySelector(".result-random")
 
+
+    if( min >= max){
+        alert("o valor min deve ser menor do que o valor maximo")
+        resultRandom.innerHTML = 0
+        
+    } else{
+
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
+        resultRandom.innerHTML = result
+    }
+   
+        
+   
     const result = Math.floor(Math.random() * (max - min + 1)) + min;
     
     
-    resultRandom.innerHTML = result
+    
 
 }
+
